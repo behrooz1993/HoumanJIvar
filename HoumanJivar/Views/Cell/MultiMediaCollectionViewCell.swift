5a64305ea22fe45a885594d616bda22977615eee
Behrooz Payoon <behrooz.payoon@gmail.com>
2018-10-25 21:15:33 +0330

    Add play button

--- HoumanJivar/Views/Cell/MultiMediaCollectionViewCell.swift
+++ HoumanJivar/Views/Cell/MultiMediaCollectionViewCell.swift
@@ -5,7 +5,6 @@ import MediaPlayer
 
 class MultiMediaCollectionViewCell: UICollectionViewCell {
  
-//    var playerLayer:AVPlayerLayer!
     var playerView:PlayerViewClass!
     var imageView:UIImageView!
     var playImageView:UIImageView!
@@ -30,10 +29,6 @@ class MultiMediaCollectionViewCell: UICollectionViewCell {
     }
     
     private func showVideo(url:String) {
-        if self.imageView != nil {
-            self.imageView.isHidden = true
-        }
-        
         self.playerView = PlayerViewClass()
         let player = AVPlayer(url: URL(string: url)!)
         player.automaticallyWaitsToMinimizeStalling = false
@@ -42,48 +37,31 @@ class MultiMediaCollectionViewCell: UICollectionViewCell {
         self.playerView.snp.makeConstraints { (make) in
             make.edges.equalTo(self.contentView)
         }
-//        self.playerView.player?.play()
         
-//
-//        self.playerLayer = AVPlayerLayer(player: player)
-//        playerLayer.frame = self.contentView.frame
-//        self.contentView.layer.addSublayer(playerLayer)
-        
-//        player.addObserver(self, forKeyPath: "status", options:NSKeyValueObservingOptions(), context: nil)
-//        player.addObserver(self, forKeyPath: "playbackBufferEmpty", options:NSKeyValueObservingOptions(), context: nil)
-//        player.addObserver(self, forKeyPath: "playbackLikelyToKeepUp", options:NSKeyValueObservingOptions(), context: nil)
-//        player.addObserver(self, forKeyPath: "loadedTimeRanges", options: NSKeyValueObservingOptions(), context: nil)
+        self.playImageView = UIImageView(image: UIImage(named: "PlayIcon"))
+        self.contentView.addSubview(self.playImageView)
+        self.playImageView.snp.makeConstraints { (make) in
+            make.width.equalTo(25)
+            make.height.equalTo(25)
+            make.center.equalTo(self.contentView.center)
+        }
     }
     
     private func showImageView() {
-//        if self.playerLayer != nil {
-//            self.playerLayer.removeFromSuperlayer()
-//        }
         self.imageView = UIImageView(frame: self.contentView.frame)
         let url = URL(string: "http://www.jbhwheelchair.com/wp-content/uploads/2016/05/novideo.png")!
         self.imageView.kf.setImage(with: url)
         self.contentView.addSubview(self.imageView)
     }
     
-//    override  func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-//        if keyPath == "status" {
-//            let status = self.playerLayer.player?.status
-//            if status == AVPlayerStatus.unknown {
-//                let x = ""
-//            }else if status == AVPlayerStatus.readyToPlay {
-//                let x = ""
-//            }else if status == AVPlayerStatus.failed {
-//                let x = ""
-//            }
-//        }
-//    }
-    
     @objc func contentViewPressed(_ sender:UITapGestureRecognizer) {
         if self.isPlaying {
             self.playerView.playerLayer.player?.pause()
+            self.playImageView.isHidden = false
             self.isPlaying = false
         }else {
             self.playerView.playerLayer.player?.play()
+            self.playImageView.isHidden = true
             self.isPlaying = true
         }
     }
